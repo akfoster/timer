d340d08fc9446905840ad8f9ead01a1f2081891a
Amy Foster <akfoster_2001@yahoo.com>
2014-07-19 18:39:28 -0700

    added list of items and ability to add a new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :start_date, :start_time, :end_time)
+  end
+
 end
